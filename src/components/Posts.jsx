9e20a995157e1978a5796612b06308391251eb59
Ja <ja.techup@gmail.com>
2024-06-06 17:43:08 +0700

    fix: moved state out of .map

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,22 +2,23 @@ import posts from "./data";
 import { useState } from "react";
 
 function Posts() {
+  const [postsState, setPostsState] = useState(posts);
+
+  const handleClick = (button, index) => {
+    const newPosts = [...postsState];
+    if (button === "like") {
+      newPosts[index].likes++;
+    } else if (button === "dislike" && newPosts[index].likes > 0) {
+      newPosts[index].likes--;
+    }
+    setPostsState(newPosts);
+  };
+
   return (
     <div className="app-wrapper">
       <h1 className="app-title">Posts</h1>
 
-      {posts.map((post) => {
-        const [likeCount, setLikeCount] = useState(post.likes);
-
-        const handleLikeClick = () => {
-          setLikeCount(likeCount + 1);
-        };
-        const handleDislikeClick = () => {
-          if (likeCount > 0) {
-            setLikeCount(likeCount - 1);
-          }
-        };
-
+      {postsState.map((post, index) => {
         return (
           <div className="post-list" key={post.id}>
             <div className="post-item">
@@ -25,15 +26,25 @@ function Posts() {
                 <h2>{post.title}</h2>
                 <div className="post-social-media-stats">
                   <span className="stats-topic">Likes: </span>
-                  <span className="post-likes">{likeCount}</span>
+                  <span className="post-likes">{post.likes}</span>
                 </div>
               </div>
               <p className="post-content">{post.content}</p>
               <div className="post-actions">
-                <button className="like-button" onClick={handleLikeClick}>
+                <button
+                  className="like-button"
+                  onClick={() => {
+                    handleClick("like", index);
+                  }}
+                >
                   Like
                 </button>
-                <button className="dislike-button" onClick={handleDislikeClick}>
+                <button
+                  className="dislike-button"
+                  onClick={() => {
+                    handleClick("dislike", index);
+                  }}
+                >
                   Dislike
                 </button>
               </div>
