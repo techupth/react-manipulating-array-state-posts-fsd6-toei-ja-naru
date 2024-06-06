ca95f767cf295844aaa4f6b1b0d26ad9530f2f05
Athikhom_ra <athikhom_ra@outlook.com>
2024-06-06 15:02:40 +0700

    Assignment 'react manipulating array state 2' NARu

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,29 +1,54 @@
+import PostsData from "./PostsData";
+import { useState } from "react";
 function Posts() {
+  const InsertPost = (post, index) => {
+    const [like, setLike] = useState(post.likes);
+    const handleLike = (e, i) => {
+      e.preventDefault();
+      post.likes++;
+      setLike(post.likes);
+      // console.log(post.likes);
+    };
+    const handleDislike = (e, i) => {
+      e.preventDefault();
+      if (post.likes <= 0) {
+        alert("It cann't to decreasing likes,Like is zero!!!");
+      } else {
+        post.likes--;
+        setLike(post.likes);
+      }
+    };
+    return (
+      <div class="post-item" key={post.id}>
+        <div class="post-header">
+          <h2>{post.title}</h2>
+          <div class="post-social-media-stats">
+            <span class="stats-topic">Likes: </span>
+            <span class="post-likes">{like}</span>
+          </div>
+        </div>
+        <p class="post-content">{post.content}</p>
+        <div class="post-actions">
+          <button
+            class="like-button"
+            onClick={(e, index) => {
+              handleLike(e, index);
+            }}
+          >
+            Like
+          </button>
+          <button class="dislike-button" onClick={handleDislike}>
+            Dislike
+          </button>
+        </div>
+      </div>
+    );
+  };
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
-      </div>
+      <div class="post-list"></div>
+      {PostsData.map(InsertPost)}
     </div>
   );
 }
