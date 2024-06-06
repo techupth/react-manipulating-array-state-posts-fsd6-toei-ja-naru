fa03fa89db73fbc62b204e642ae8c2277792a9ac
Puttapong <puttapong.lekk@gmail.com>
2024-06-06 14:42:03 +0700

    fix:(src/Post.jsx):render post title

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 function Posts() {
   const [postList, setPostList] = useState([...PostData]);
 
-  const likeStatArray = postList.map((item) => item.likes);
-  const [likes, setlikes] = useState([...likeStatArray]);
+  const likesStatArray = postList.map((item) => item.likes);
+  const [likes, setlikes] = useState([...likesStatArray]);
 
   const handleLike = (like, i) => {
     const newLikes = [...likes];
@@ -25,7 +25,9 @@ function Posts() {
           return (
             <div class="post-item" key={index}>
               <div class="post-header">
-                <h2>Post Title #{item.id}</h2>
+                <h2>
+                  {item.title} #{item.id}
+                </h2>
                 <div class="post-social-media-stats">
                   <span class="stats-topic">Likes: </span>
                   <span class="post-likes"> {likes[index]} </span>
